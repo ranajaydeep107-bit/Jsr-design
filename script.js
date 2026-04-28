fc2e0e78e4232956b06f12cde54f651be60f857f
rana <ranajaydeep107@gmail.com>
2026-04-28 17:32:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Design Studio Website Loaded");
+// LOADER
+window.onload = () => {
+setTimeout(()=>{
+document.getElementById("loader").style.display="none";
+},1800);
+};
+
+// PARTICLES BACKGROUND
+const canvas=document.getElementById("particles");
+const ctx=canvas.getContext("2d");
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+let particles=[];
+
+for(let i=0;i<120;i++){
+particles.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+r:Math.random()*2
+});
+}
+
+function animate(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+particles.forEach(p=>{
+ctx.beginPath();
+ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
+ctx.fillStyle="#00ffd5";
+ctx.fill();
+
+p.y+=0.3;
+if(p.y>canvas.height)p.y=0;
+});
+
+requestAnimationFrame(animate);
+}
+
+animate();
